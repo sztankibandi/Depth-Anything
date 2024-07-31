709debf8e6946a1e23d2a1169de8bf9e4211b2bc
sztankibandi <sztankibandi@gmail.com>
2024-07-31 13:26:30 +0200

    troubleshhot forkserver + config

--- metric_depth/train_mono.py
+++ metric_depth/train_mono.py
@@ -39,6 +39,8 @@ import os
 os.environ["PYOPENGL_PLATFORM"] = "egl"
 os.environ["WANDB_START_METHOD"] = "thread"
 
+from zoedepth.utils.easydict import EasyDict as edict
+
 
 def fix_random_seed(seed: int):
     import random
@@ -112,7 +114,7 @@ def main_worker(gpu, ngpus_per_node, config):
 
 
 if __name__ == '__main__':
-    mp.set_start_method('forkserver')
+    mp.set_start_method('spawn')
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--model", type=str, default="synunet")
@@ -126,7 +128,101 @@ if __name__ == '__main__':
     if args.trainer is not None:
         overwrite_kwargs["trainer"] = args.trainer
 
-    config = get_config(args.model, "train", args.dataset, **overwrite_kwargs)
+    config = {
+        'attractor_alpha': 1000,
+        'attractor_gamma': 2,
+        'attractor_kind': 'mean',
+        'attractor_type': 'inv',
+        'aug': True,
+        'avoid_boundary': False,
+        'batch_size': 16,
+        'bin_centers_type': 'softplus',
+        'bin_embedding_dim': 128,
+        'bs': 16,
+        'clip_grad': 0.1,
+        'cycle_momentum': True,
+        'data_path': './data\\nyu',
+        'data_path_eval': './data\\nyu',
+        'dataset': 'nyu',
+        'degree': 1.0,
+        'dist_backend': 'nccl',
+        'dist_url': 'tcp://127.0.0.1:15004',
+        'distributed': True,
+        'div_factor': 1,
+        'do_kb_crop': False,
+        'do_random_rotate': True,
+        'eigen_crop': True,
+        'encoder_lr_factor': 50,
+        'epochs': 5,
+        'filenames_file': './train_test_inputs/nyudepthv2_train_files_with_gt.txt',
+        'filenames_file_eval': './train_test_inputs/nyudepthv2_test_files_with_gt.txt',
+        'final_div_factor': 10000,
+        'freeze_midas_bn': True,
+        'garg_crop': False,
+        'gpu': None,
+        'gt_path': './data\\nyu',
+        'gt_path_eval': './data\\nyu',
+        'img_size': [392, 518],
+        'input_height': 480,
+        'input_width': 640,
+        'inverse_midas': False,
+        'log_images_every': 0.1,
+        'lr': 0.000161,
+        'max_depth': 10,
+        'max_depth_diff': 10,
+        'max_depth_eval': 10,
+        'max_temp': 50.0,
+        'max_translation': 100,
+        'memory_efficient': True,
+        'midas_lr_factor': 50,
+        'midas_model_type': 'DPT_BEiT_L_384',
+        'min_depth': 0.001,
+        'min_depth_diff': -10,
+        'min_depth_eval': 0.001,
+        'min_temp': 0.0212,
+        'mode': 'train',
+        'model': 'zoedepth',
+        'n_attractors': [16, 8, 4, 1],
+        'n_bins': 64,
+        'name': 'ZoeDepth',
+        'ngpus_per_node': 0,
+        'notes': '',
+        'num_workers': 16,
+        'output_distribution': 'logbinomial',
+        'pct_start': 0.7,
+        'pos_enc_lr_factor': 50,
+        'prefetch': False,
+        'pretrained_resource': '.\\checkpoints\\depth_anything_vitl14.pth',
+        'print_losses': False,
+        'project': 'MonoDepth3-nyu',
+        'random_crop': False,
+        'random_translate': False,
+        'rank': 0,
+        'root': '.',
+        'same_lr': False,
+        'save_dir': './depth_anything_finetune',
+        'shared_dict': None,
+        'tags': '',
+        'three_phase': False,
+        'train_midas': True,
+        'trainer': 'zoedepth',
+        'translate_prob': 0.2,
+        'uid': None,
+        'use_amp': False,
+        'use_pretrained_midas': True,
+        'use_shared_dict': False,
+        'validate_every': 0.25,
+        'version_name': 'v1',
+        'w_domain': 0.2,
+        'w_grad': 0,
+        'w_reg': 0,
+        'w_si': 1,
+        'wd': 0.01,
+        'workers': 16,
+        'world_size': 1
+        }
+    config = edict(config)
+    # config = get_config(args.model, "train", args.dataset, **overwrite_kwargs)
     # git_commit()
     if config.use_shared_dict:
         shared_dict = mp.Manager().dict()
@@ -170,6 +266,7 @@ if __name__ == '__main__':
     pprint(config)
     if config.distributed:
         config.world_size = ngpus_per_node * config.world_size
+        print(ngpus_per_node)
         mp.spawn(main_worker, nprocs=ngpus_per_node,
                  args=(ngpus_per_node, config))
     else:
